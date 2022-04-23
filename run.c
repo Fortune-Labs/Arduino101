a1b0b5fd8c9f3d28f11f3feefd1a50daaa801a71
Fortune <wulnyefortunatus@gmail.com>
2022-04-23 13:21:56 +0000

    link

---
+++ run.c
@@ -0,0 +1,5 @@
+dsfwdsfbld
+'jdsfbsd
+jdsfsdf
+sdfjdsf
+'
--- run1.c
+++ run1.c
@@ -1,1 +1,2 @@
 printf("Hello World");
+https://dev.to/narottam04/step-by-step-guide-how-to-set-up-visual-studio-code-for-c-and-c-programming-2021-1f0i
